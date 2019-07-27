c0c539be201c4484cfd3501169dfc4cb2597ac53
Gajo Petrovic <gajo01@gmail.com>
2019-07-27 17:10:44 +0900

    add modinfo.lua

---
+++ modinfo.lua
@@ -0,0 +1,12 @@
+return {
+  name = 'EvoRTS Chobby',
+  shortName = 'EvoRTS_CHOBBY',
+  description = 'EvoRTS mutator for Chobby',
+  version = '$VERSION',
+  mutator = 'Official',
+  modtype = 5,
+  onlyLocal = true,
+  depend = {
+      'rapid://chobby:test',
+  },
+}
