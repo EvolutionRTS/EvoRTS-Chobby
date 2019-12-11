96db2e9d5522f1c65e6f473f866e322b0fa54c49
ForbodingAngel <forbodingangel@gmail.com>
2019-12-10 21:31:18 -0800

    I wish there were a faster way to test this stuff

--- LuaMenu/widgets/snd_music_lite.lua
+++ LuaMenu/widgets/snd_music_lite.lua
@@ -46,8 +46,9 @@ end
 --------------------------------------------------------------------------------
 
 local function StartTrack(trackName, snd_volmusic)
+	snd_volmusic = Spring.GetConfigInt("snd_volmusic")
 	trackName = trackName or GetRandomTrack(previousTrack)
-	Spring.Echo("Starting Track", trackName, snd_volmusic)
+	Spring.Echo("[Lobby Music Player] Starting Track", trackName, snd_volmusic)
 	if snd_volmusic == 0 then
 		return
 	end
@@ -153,6 +154,7 @@ function widget:Initialize()
 	if musicInitialValue ~= 1 then
 		Spring.SetConfigInt("snd_volmusic", 20)
 		Spring.SetConfigInt("evo_musicInitialValue", 1)
+		Spring.Echo ("[Lobby Music Player] Setting initial music volume"
 	end
 
 	snd_volmusic = Spring.GetConfigInt("snd_volmusic")
